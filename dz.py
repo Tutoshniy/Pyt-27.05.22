941c71e9fecdca664577ceb28e1a5047d0f63972
Gleb <gleb.gadbrat@mail.ru>
2022-05-27 19:06:15 +0300

    Задание 1 и 2

---
+++ dz.py
@@ -0,0 +1,21 @@
+#1 Найти сумму чисел списка стоящих на нечетной позиции
+#list = []
+# sum = 0
+def vvod(): #функция заполнения массива
+    n = int(input("Введите количество чисел: "))
+    print("Вводите числа")
+    for i in range(n):
+        list.append(int(input()))
+# vvod()
+# print(list)
+# for i in range(len(list)):
+#     if i%2 == 0:                  решение первого номера
+#         sum+=list[i]              функция я использовал в следующих номерах
+# print(sum)
+
+# list = []                         Решение второго номера
+# result = []
+# vvod()
+# for i in range(len(list)//2+len(list)%2):
+#     result.append(list[i] * list[len(list)-i-1])
+# print(result)
